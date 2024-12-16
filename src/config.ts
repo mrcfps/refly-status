61f4b462893f9a56d83231c5b14fcce74a0806fc
mrcfps <mrc@powerformer.com>
2024-12-16 23:20:40 +0800

    Update config

--- src/config.ts
+++ src/config.ts
@@ -2,42 +2,22 @@ import type { Config } from './types'
 
 export const config: Config = {
   settings: {
-    title: 'Status Page Pro',
-    url: 'https://cf-worker-status-page-pro-production.yunsii.workers.dev',
+    title: 'Refly Status',
+    url: 'https://refly-status.powerformer.workers.dev',
     displayDays: 90,
     collectResponseTimes: true,
   },
   monitors: [
     {
-      id: 'workers.cloudflare.com',
-      url: 'https://workers.cloudflare.com',
-      description: 'You write code. They handle the rest.',
-      followRedirect: false,
+      id: 'refly.ai',
+      url: 'https://refly.ai',
+      description: 'Refly website',
     },
     {
-      id: 'www.cloudflare.com',
-      url: 'https://www.cloudflare.com',
-      description: 'Built for anything connected to the Internet.',
-    },
-    {
-      id: 'blog.cloudflare.com',
-      url: 'https://blog.cloudflare.com',
-      name: 'The Cloudflare Blog',
-    },
-    {
-      id: 'google',
-      url: 'https://www.google.com/',
-      name: 'Google',
-      followRedirect: true,
-    },
-    {
-      id: 'bilibili',
-      url: 'https://www.bilibili.com/',
-    },
-    {
-      id: 'GitHub',
-      url: 'https://github.com/',
+      id: 'api.refly.ai',
+      url: 'https://api.refly.ai',
+      description: 'Refly API endpoint',
     },
   ],
-  monitorsCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSnewwW9OuXgtuutyYSfFJ_AZdI-UpkUjP2wWi-zZWM3MKa8IzBceWCe9qB_-Lmk-S7mSFgqKVnokam/pub?gid=0&single=true&output=csv',
+  // monitorsCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSnewwW9OuXgtuutyYSfFJ_AZdI-UpkUjP2wWi-zZWM3MKa8IzBceWCe9qB_-Lmk-S7mSFgqKVnokam/pub?gid=0&single=true&output=csv',
 }
